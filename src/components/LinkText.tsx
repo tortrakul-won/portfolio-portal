6383d347327371c43fe06fe618d93f05a688519b
tortrakul-won <tortrakulwonglieng@gmail.com>
2023-07-31 15:12:05 +0700

    left panel layout

---
+++ src/components/LinkText.tsx
@@ -0,0 +1,16 @@
+export default function LinkText({
+  textColor,
+  children,
+}: {
+  textColor: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <h2
+      className="w-fit text-4xl origin-left hover:scale-[1.10] ease-in-out duration-100 active:hover:scale-90 "
+      style={{ color: textColor }}
+    >
+      {children}
+    </h2>
+  );
+}
