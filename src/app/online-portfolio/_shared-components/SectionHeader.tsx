7b01d0bdd7a6b27ec3631fc73e366393a1c61b0d
tortrakul-won <tortrakulwonglieng@gmail.com>
2026-08-06 13:06:24 +0700

    Add small top margin before section headers on desktop too

--- src/app/online-portfolio/_shared-components/SectionHeader.tsx
+++ src/app/online-portfolio/_shared-components/SectionHeader.tsx
@@ -1,7 +1,7 @@
 export default function SectionHeader({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <h2 className="text-xl lg:text-2xl text-white bg-charcoal w-full text-center py-1 mt-4 lg:mt-0">{children}</h2>
+      <h2 className="text-xl lg:text-2xl text-white bg-charcoal w-full text-center py-1 mt-4 lg:mt-2">{children}</h2>
     </>
   );
 }
